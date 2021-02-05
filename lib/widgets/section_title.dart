dd4bff084406583c8dc8999be5fa53c2cd23ef3e
Adem ÖZCAN <ozcanfurkan@gmail.com>
2021-02-05 16:22:01 +0300

    SectionTitle changed to StatelessWidget

--- lib/widgets/section_title.dart
+++ lib/widgets/section_title.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
-Widget sectionTitle(String title, {bool isPrimary = true}) {
-  return Padding(
-    padding: EdgeInsets.symmetric(vertical: 70.0),
-    child: Text(
-      title,
-      style: TextStyle(fontWeight: FontWeight.w900, fontSize: 48, color: isPrimary ? Colors.black : Colors.white),
-    ),
-  );
+class SectionTitle extends StatelessWidget {
+  final String title;
+  final Color textColor;
+
+  const SectionTitle({@required this.title, this.textColor = Colors.black});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 70.0),
+      child: Text(
+        title,
+        style: TextStyle(fontWeight: FontWeight.w800, fontSize: 48, color: textColor),
+      ),
+    );
+  }
 }
