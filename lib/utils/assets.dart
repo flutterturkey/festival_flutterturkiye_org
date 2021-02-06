0f9980afc09b6891722cd135d242a9f1bc02188c
Adem ÖZCAN <ozcanfurkan@gmail.com>
2021-02-06 15:23:47 +0300

    logo added

--- lib/utils/assets.dart
+++ lib/utils/assets.dart
@@ -1,5 +1,5 @@
 class Assets {
-  static const String logo = 'assets/images/flutter_tr.png';
+  static const String logo = 'assets/images/logo.png';
   static const String footerBackground = 'assets/images/footer_bg.jpg';
   static const String speakerPlaceHolder =
       'assets/images/speaker_placeholder.png';
