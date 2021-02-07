0d52bc7f1fd6526f5fb4eb2688c0e0fc0a78ab9c
Göktuğ Özdemir <bgoktugozdemir@gmail.com>
2021-02-07 14:33:11 +0300

    Update ThemeHelper
    * lightBlueColor's name has changed to blueColor.
    * speakerDetailImageBorder has added.

--- lib/core/styles/theme_helper.dart
+++ lib/core/styles/theme_helper.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 class ThemeHelper {
   static const Color darkColor = Color(0xFF212937);
   static const Color lightColor = Color(0xFFFFFFFF);
-  static const Color lightBlueColor = Color(0xFF07C6F9);
+  static const Color blueColor = Color(0xFF07C6F9);
   static const Color activityFlowColor = Color(0xFFF2F2F2);
   static const Color eventPointColor = Color(0xFFFF9B05);
+  static const Color speakerDetailImageBorder = Color(0xFFFF7A00);
   static const Color cardBackgroundColor = Color(0xFF1C1C1D);
   static const Color footerTextColor = Color(0xFFD9DBE1);
   static const Brightness brightness = Brightness.light;
