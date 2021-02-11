0a5e050195c4b1159387a53e131b172c2e0d6ca1
Adem ÖZCAN <ozcanfurkan@gmail.com>
2021-02-11 12:58:19 +0300

    hotfix

--- lib/widgets/footer_section.dart
+++ lib/widgets/footer_section.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:hackathon_flutterturkiye_org/utils/assets.dart';
-import 'package:hackathon_flutterturkiye_org/widgets/responsive_builder.dart';
+import 'package:festival_flutterturkiye_org/utils/assets.dart';
+import 'package:festival_flutterturkiye_org/widgets/responsive_builder.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -416,102 +416,3 @@ class SocialIcon extends StatelessWidget {
     );
   }
 }
-
-
-    /* return Align(
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        padding: const EdgeInsets.only(top: 20),
-        height: MediaQuery.of(context).size.height / 2,
-        width: MediaQuery.of(context).size.width,
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(Assets.footerBackground),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ), // background
-            Container(
-              color: Colors.black.withOpacity(0.75),
-            ), // black filter
-            Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      top: 40.0,
-                    ),
-                    child: Image.asset(
-                      Assets.logo,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30, bottom: 10),
-                    child: InkWell(
-                      onTap: () =>
-                          launch('mailto:hackathon@flutterturkiye.org'),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.mail,
-                            color: Colors.white,
-                          ),
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 10.0),
-                              child: Text(
-                                'hackathon@flutterturkiye.org',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 24,
-                                  fontFamily: 'Montserrat',
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Divider(
-                    indent: 100,
-                    endIndent: 100,
-                    color: Colors.white,
-                    thickness: 0.5,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: MediaQuery.of(context).size.width > 1200
-                          ? const EdgeInsets.only(
-                              top: 10,
-                              left: 100,
-                              right: 100,
-                            )
-                          : const EdgeInsets.only(
-                              top: 10,
-                              left: 10,
-                            ),
-                      child: Text(
-                        '© 2021 Flutter Türkiye',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                  ),
-                  BottomView(),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    ); */
