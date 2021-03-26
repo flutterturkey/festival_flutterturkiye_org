410a924965fefcfa886c4f9d6a5336db2adf4299
Göktuğ Özdemir <bgoktugozdemir@gmail.com>
2021-03-26 22:47:12 +0300

    EventFlow has been closed.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -80,7 +80,6 @@ class _HomePageState extends State<HomePage> {
               controller: _scrollController,
               children: const <Widget>[
                 CountdownSection(),
-                EventFlowSection(),
                 FooterSection(),
               ],
             ),
