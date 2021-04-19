aee212bab3012c289055b742d76f1cca192870b3
Göktuğ Özdemir <bgoktugozdemir@gmail.com>
2021-04-19 17:47:41 +0300

    Removing unused imports

--- lib/event_flow/ui/session_info_field.dart
+++ lib/event_flow/ui/session_info_field.dart
@@ -1,8 +1,3 @@
-import 'dart:convert';
-import 'dart:html' as html;
-import 'dart:io';
-
-import 'package:festival_flutterturkiye_org/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
