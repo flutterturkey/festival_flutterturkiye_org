84675f43de8dcaad5af9c47749f14f4b64b10218
bgoktugozdemir <bgoktugozdemir@users.noreply.github.com>
2022-03-14 11:07:05 +0000

    deploy: 4c8ae83a5cf90f16374e6909b68e885d21b6532d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -56,7 +56,7 @@ const RESOURCES = {
 "version.json": "e9c992da5fbf95ff89dfa34521a8e9d8",
 "icons/Icon-512.png": "939bbe74c06923f54a9c48805e72c498",
 "icons/Icon-192.png": "f734be520ccdb09c10e2bbc6ec0b191b",
-"main.dart.js": "e33b8656da4ba66ddfda16514c2bc470",
+"main.dart.js": "655d3c86d0a815246e22196f22531f10",
 "favicon.png": "984542880c9a626c99c8984baa3f5c29",
 "manifest.json": "21ec26c3d3fde1fd00fb3b188a7bc64d",
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
